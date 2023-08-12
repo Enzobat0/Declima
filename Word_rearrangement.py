22fed2808535b1868c88a2fc7ad57c80ffc14807
Enzo Bat <e.batungwan@alustudent.com>
2023-08-12 12:29:08 +0200

    first try

---
+++ Word_rearrangement.py
@@ -0,0 +1,55 @@
+import random
+
+def load_words(filename):
+    with open(filename, 'r') as file:
+        return [word.strip() for word in file.readlines()]
+
+def pick_random_word(words_list):
+    return random.choice(words_list)
+
+def rearrange_letters(word):
+    letters = list(word)
+    random.shuffle(letters)
+    return ''.join(letters)
+
+def is_word_valid(guess, correct_word):
+    return guess.lower() == correct_word.lower()
+
+def game():
+    words_to_guess = load_words("words.txt")
+    definitions = {}
+    with open("definitions.txt", 'r') as file:
+        for line in file:
+            word, definition = line.strip().split(':', 1)
+            definitions[word] = definition.strip()
+
+    print("Welcome to the Word Rearrangement Game!")
+
+    while True:
+        word_to_guess = pick_random_word(words_to_guess)
+        shuffled_word = rearrange_letters(word_to_guess)
+
+        print("\nYou have 3 attempts to rearrange the letters and find the correct word related to climate change.")
+        print(f"Rearrange the letters: {shuffled_word}")
+
+        for attempt in range(1, 4):
+            guess = input(f"Attempt {attempt}: ").strip()
+
+            if is_word_valid(guess, word_to_guess):
+                print("Congratulations! You found the correct word!")
+                print(f"The word '{word_to_guess}' means: {definitions[word_to_guess]}")
+                break
+            else:
+                print("Incorrect guess. Try again!")
+
+        else:
+            print(f"\nYou've used all your attempts. The correct word was '{word_to_guess}'.")
+            print(f"The word '{word_to_guess}' means: {definitions[word_to_guess]}")
+
+        choice = input("Do you want to continue? (yes/no): ").strip().lower()
+        if choice != 'yes':
+            print("Thank you for playing! Goodbye!")
+            break
+
+if __name__ == "__main__":
+    game()
---
+++ riddle.py
@@ -0,0 +1,65 @@
+import random
+
+
+def main():
+    display_instructions()
+    riddles = select_riddle()
+    play_riddle_game(riddles)
+
+
+#Displays the instructions
+def display_instructions():
+    print("Welcome to Declima riddle game!")
+    print("Instructions:")
+    print("You'll be presented with riddles related to climate change.")
+    print("You have 3 attempts to guess each riddle correctly.")
+    print("Let's get started!")
+    print("PS: Your answers should always be in lowercase.")
+
+
+#Select a random riddle in the list
+def select_riddle():
+    riddles = [
+        {"question" : "I am essential for life, but excessive amounts are causing global warming. What am I?",
+         "answer" : "carbon dioxide"},
+         {"question" : "I am melting rapidly due to rising global temperatures. What am I?",
+          "answer" : "glacier"},
+        {"question" : "I am a gas that's helpful to plants, but I cannot be bought at a gas station. What am I?",
+         "answer" : "carbon dioxide"},
+        {"question" : "I am a mixture of gases. Without me, life's existence would be rare. What am I?",
+         "answer" : "atmosphere"}
+    ]
+    return riddles  
+
+
+#start playing
+def play_riddle_game(riddles):
+    score = 0 
+
+#The for loop so it ends when all riddles have been used
+    for riddle in riddles:
+        lives_left = 3
+
+        print("\nRiddle: " + riddle["question"])
+        while lives_left > 0:
+            user_guess = input("Your Answer ({} lives left): ".format(lives_left)).lower()
+# Verify user guess, and remove one life if the user fail.            
+            if user_guess == riddle["answer"]:
+                print("Congratulations! That's the correct answer!")
+                score += 1
+                break
+            else:
+                lives_left -= 1
+                if lives_left > 0:
+                    print("Incorrect answer. Try again!")
+                else:
+                    print("GAME OVER! You've used all your lives. The correct answer was: '{}'.".format(riddle["answer"]))
+
+    print("\nThank you for playing! Your final score: {} out of {} riddles.".format(score, len(riddles)))
+
+        
+        
+
+
+if __name__ == '__main__':
+    main()
