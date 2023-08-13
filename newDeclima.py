1ee6c816192cf551d85ce2c6f6e37f47eb780d13
E.Kporon <127246453+EKporon@users.noreply.github.com>
2023-08-13 20:56:54 +0200

    Update newDeclima.py

--- newDeclima.py
+++ newDeclima.py
@@ -1,2 +1,22 @@
+import Word_rearrangement
+import riddle
+
+def display_instructions():
+    # ... (same as in the new code)
+
+def get_user_choice():
+    # ... (same as in the new code)
+
+def main():
+    display_instructions()
+    choice = get_user_choice()
+
+    if choice == '1':
+        Word_rearrangement.game()
+    elif choice == '2':
+        riddle.start_game()
+
+if __name__ == '__main__':
+    main()
 
 
